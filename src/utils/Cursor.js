4ea1f160e62f9a372a2851ec9e267891d708c3af
Joe Boylson <jboylson@saec-kv.com>
2020-10-29 15:45:24 -0400

    fixing cursor.js server-side rendering

--- src/utils/Cursor.js
+++ src/utils/Cursor.js
@@ -1,8 +1,9 @@
 const Cursor = {
+    initialized: false,
     element: null,
     timeout: null,
     init: function() {
-        if (!this.element && !typeof window === "undefined") {
+        if (!this.element && typeof window === "object") {
             let cursorSpan = document.createElement('span')
             cursorSpan.id = 'cursor-span';
             document.body.prepend(cursorSpan);
@@ -11,27 +12,28 @@ const Cursor = {
                 cursorSpan.style.transform = `translate( ${e.clientX + 21}px, ${e.clientY - (21/2)}px )`;
             })
 
-            this.element = cursorSpan
+            this.element = cursorSpan;
         }
     },
-    setText: function(text) { 
+    setText: function(text) {
+        this.init();
         if (this.element) {
             this.element.innerHTML = text;
             this.timeout = setTimeout(() => Cursor.clearText(), 3000)
         }
     },
     clearText: function() { 
+        this.init();
         if (this.element) this.element.innerHTML = null;
         clearTimeout(this.timeout);
     },
     applyBehavior: function(text) {
+        this.init();
         return {
             onMouseEnter: () => this.setText(text),
             onMouseLeave: () => this.clearText()
         }
     }
 }
-
-Cursor.init();
 
 export default Cursor;
